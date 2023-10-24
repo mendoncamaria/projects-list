d96a1528e4fe206a35904eef7821d44abe6ff71e
Maria Mendonca <maria.mendonca156@gmail.com>
2023-10-24 18:07:23 +0530

    Added projects list

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,46 @@ import Card from './components/card';
 function App() {
   const cardsData = [
     {
-      title: 'Example Card 1',
-      description: 'This is a reusable card component.',
-      language: 'JavaScript',
-      url: 'https://example.com/card1',
+      title: 'Todo List',
+      description: 'Todo List',
+      language: 'JavaScript, Firebase',
+      url: 'https://mariamendonca-todo-app.netlify.app/',
     },
     {
-      title: 'Example Card 1',
-      description: 'This is a reusable card component.',
-      language: 'JavaScript',
-      url: 'https://example.com/card1',
+      title: 'Portfolio',
+      description: 'Portfolio',
+      language: 'NextJS',
+      url: 'https://maria-mendonca.vercel.app/',
     },
     {
-      title: 'Example Card 1',
-      description: 'This is a reusable card component.',
-      language: 'JavaScript',
-      url: 'https://example.com/card1',
+      title: 'Weather',
+      description: 'Weather',
+      language: 'ReactJS',
+      url: 'https://weather-mendoncamaria.vercel.app/',
     },
     {
-      title: 'Example Card 1',
-      description: 'This is a reusable card component.',
-      language: 'JavaScript',
-      url: 'https://example.com/card1',
+      title: 'Sticky Notes',
+      description: 'Sticky notes',
+      language: 'Typescript',
+      url: 'https://stickynotes-mendoncamaria.vercel.app/',
+    },
+    {
+      title: 'Quiz',
+      description: 'Quiz',
+      language: 'ReactJS',
+      url: 'https://quiz-mendoncamaria.vercel.app/',
+    },
+    {
+      title: 'Gradient background',
+      description: 'Gradient background',
+      language: 'ReactJS',
+      url: 'https://gradientbackground-mendoncamaria.netlify.app/',
+    },
+    {
+      title: 'Temperature Conversion',
+      description: 'Temperature Conversion',
+      language: 'ReactJS',
+      url: 'https://temperatureconverter-mendoncamaria.netlify.app/',
     },
   ];
 
