d7ad26319275f43299ba05afe048c6b66cfda2f2
Maria Mendonca <maria.mendonca156@gmail.com>
2023-10-30 23:20:50 +0530

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -47,13 +47,20 @@ function App() {
   ];
 
   return (
-    <div className="bg-gradient-to-b from-gray-50 to-gray-100 min-h-screen flex flex-wrap items-center justify-center p-8">
-      {cardsData.map((card, index) => (
-        <div key={index} className="w-full sm:w-1/2 md:w-1/4 p-2">
-          <Card {...card} />
+    <>
+      <div className="wrapper">
+        <div className="bg-gradient-to-b from-gray-50 to-gray-100 min-h-screen flex flex-wrap items-center justify-center p-8">
+          {cardsData.map((card, index) => (
+            <div key={index} className="w-full sm:w-1/2 md:w-1/4 p-2">
+              <Card {...card} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+      <footer className="bg-black text-white text-center py-4">
+        &copy; 2023 Maria Mendonca
+      </footer>
+    </>
   );
 }
 
