2c9340d808061ac6040f145ea576fef36d0efaf1
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-03 07:55:45 +0530

    moved project details array to separate file

---
+++ src/data/arrayConstants.ts
@@ -0,0 +1,44 @@
+export const cardsData = [
+  {
+    title: 'Todo List',
+    description: 'Todo List',
+    language: 'JavaScript, Firebase',
+    url: 'https://mariamendonca-todo-app.netlify.app/',
+  },
+  {
+    title: 'Portfolio',
+    description: 'Portfolio',
+    language: 'NextJS',
+    url: 'https://maria-mendonca.vercel.app/',
+  },
+  {
+    title: 'Weather',
+    description: 'Weather',
+    language: 'ReactJS',
+    url: 'https://weather-mendoncamaria.vercel.app/',
+  },
+  {
+    title: 'Sticky Notes',
+    description: 'Sticky notes',
+    language: 'Typescript',
+    url: 'https://stickynotes-mendoncamaria.vercel.app/',
+  },
+  {
+    title: 'Quiz',
+    description: 'Quiz',
+    language: 'ReactJS',
+    url: 'https://quiz-mendoncamaria.vercel.app/',
+  },
+  {
+    title: 'Gradient background',
+    description: 'Gradient background',
+    language: 'ReactJS',
+    url: 'https://gradientbackground-mendoncamaria.netlify.app/',
+  },
+  {
+    title: 'Temperature Conversion',
+    description: 'Temperature Conversion',
+    language: 'ReactJS',
+    url: 'https://temperatureconverter-mendoncamaria.netlify.app/',
+  },
+];
