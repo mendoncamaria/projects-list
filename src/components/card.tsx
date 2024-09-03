bf0e6c8707de2179b1a844a24142f400ddedc334
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-03 08:26:53 +0530

    Updated card design

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,14 +8,34 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ title, description, language, url }) => {
+  const [showDescription, setshowDescription] = React.useState(false);
+
   return (
-    <div className="bg-white rounded-md shadow-md p-6 h-64">
-      <h2 className="text-xl font-semibold mb-4">{title}</h2>
-      <p className="text-gray-600 mb-2">{description}</p>
-      <p className="text-gray-500 mb-2">{`Language: ${language}`}</p>
-      <a href={url} className="text-blue-500 hover:underline" target='_blank'>
-        View details
-      </a>
+    <div className="bg-white rounded-md shadow-md p-6">
+      <h2 className="text-xl font-semibold mb-4 text-center">{title}</h2>
+      <div>
+        <p className="text-gray-500 mb-2">{`Language: ${language}`}</p>
+      </div>
+      <div className="flex justify-center gap-4">
+        <a href={url} className="text-blue-500 hover:underline" target="_blank">
+          Github
+        </a>
+        <a href={url} className="text-blue-500 hover:underline" target="_blank">
+          Live Link
+        </a>
+      </div>
+      <div>
+        <p className="text-gray-600 mb-2">
+          {showDescription ? description : ''}
+        </p>
+
+        <button
+          onClick={() => setshowDescription(!showDescription)}
+          className="text-blue-500 hover:underline"
+        >
+          {showDescription ? 'View Less' : 'View More'}
+        </button>
+      </div>
     </div>
   );
 };
