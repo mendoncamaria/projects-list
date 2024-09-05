a54631c4d40bef377845bd0f854d791a2b6fb510
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-05 14:45:49 +0530

    updated bg color, heading text

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ function App() {
   return (
     <>
       <div className="wrapper">
-        <div className="bg-gradient-to-b from-gray-50 to-gray-100 items-center justify-center p-8 height-style">
-          <h1 className="text-3xl font-display text-center mb-8">
+        <div className="bg-zinc-600 items-center justify-center p-8 height-style">
+          <h1 className="text-4xl font-mono font-semibold text-center mb-8 text-white">
             PROJECT DASHBOARD
           </h1>
 
           <div className="relative w-full rounded-lg overflow-hidden ">
-            <div className="bg-white w-full p-8 rounded-lg z-10 shadow-md flex flex-wrap">
+            <div className="bg-gradient-to-b from-zinc-200 to-zinc-300 w-full p-8 rounded-lg z-10 shadow-md flex flex-wrap">
               {cardsData.map((card, index) => (
                 <div key={index} className="w-full sm:w-1/2 md:w-1/4 p-2">
                   <Card {...card} />
