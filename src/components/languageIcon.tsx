0907b1c052b7058eade50a2ebb6f82f7b653be16
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-06 09:38:47 +0530

    updated language design

--- src/components/languageIcon.tsx
+++ src/components/languageIcon.tsx
@@ -1,18 +1,26 @@
+import React from 'react';
+
 interface Language {
   name: string;
   filename: string;
 }
 
-interface LanguageIconProps {
-  languages: Language[];
-}
-
-export const LanguageIcon: React.FC<LanguageIconProps> = ({ languages }) => {
+const LanguageIcon: React.FC<{ languages: Language[] }> = ({ languages }) => {
   return (
-    <div className="flex">
-      {languages.map((language) => (
-        <img src={language.filename} alt={language.name} key={language.name} />
-      ))}
+    <div className="flex gap-2 items-center">
+      <h3 className="text-gray-700 font-semibold">I'm built using</h3>
+      <div className="flex gap-1">
+        {languages.map((language) => (
+          <span
+            key={language.filename}
+            className="inline-flex rounded-full px-2 py-1 text-center text-gray-700 bg-gray-200 mr-1"
+          >
+            {language.name}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
+
+export default LanguageIcon;
