eba0bccd73c4ffe8f0a22fdc0b0cf0b8de6f6423
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-10 09:54:44 +0530

    added heading size

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -22,7 +22,7 @@ const Card: React.FC<CardProps> = ({ title, description, language, liveUrl, gitU
 
   return (
     <div className="bg-white rounded-md shadow-md p-6 flex flex-col gap-4">
-      <h1 className="text-3xl font-semibold font-serif mb-4 text-center">{title}</h1>
+      <h1 className="2xl:text-3xl xl:text-2xl text-3xl font-semibold font-serif mb-4 text-center">{title}</h1>
       <div className="flex justify-center items-center">
         <LanguageIcon languages={language} />
       </div>
