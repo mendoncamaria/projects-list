25f4fd5ad59e3c226ab8fcdce17ab4638ea69490
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-10 09:54:55 +0530

    imported page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import Card from './components/card';
-import { cardsData } from './data/arrayConstants';
+// import Card from './components/card';
+import Page from './container/Page';
+// import { cardsData } from './data/arrayConstants';
 
 function App() {
   return (
     <>
       <div className="wrapper">
-        <div className="bg-zinc-600 items-center justify-center p-8 height-style">
-          <h1 className="text-4xl font-mono font-semibold text-center mb-8 text-white">
-            PROJECT DASHBOARD
-          </h1>
-
-          <div className="relative w-full rounded-lg overflow-hidden ">
-            <div className="bg-gradient-to-b from-zinc-200 to-zinc-300 w-full p-8 rounded-lg z-10 shadow-md flex flex-wrap">
-              {cardsData.map((card, index) => (
-                <div key={index} className="w-full sm:w-1/2 md:w-1/4 p-2">
-                  <Card {...card} />
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
+        <Page />
         <footer className="bg-black text-white text-center py-4">
           &copy;2024{' '}
           <a href="https://maria-mendonca.vercel.app/" target="_self">
